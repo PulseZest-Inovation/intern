91c791bb28e9edd119e6422a8138cfd8c0165743
Ambikesh <abhay2004raj15@gmail.com>
2024-03-14 23:39:08 +0530

    dark mode enabled

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+const darkModeToggle = document.getElementById('darkModeToggle');
+
+darkModeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+});
