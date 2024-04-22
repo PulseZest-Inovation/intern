38f52e3fdc9631872b3ebb0d567eb16b0eb9d138
unknown <nafeesahadbly@gmail.com>
2024-04-23 00:03:51 +0530

    update extra space and button

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ const darkModeToggle = document.getElementById('darkModeToggle');
 darkModeToggle.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
 });
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("btnBackToTop").style.display = "block";
+  } else {
+    document.getElementById("btnBackToTop").style.display = "none";
+  }
+}
+
+function scrollToTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
